fa2526d904671ee7f454752679f7d54ebe702b57
Romain-Chevrollier <romain2002.chevrollier@gmail.com>
2023-05-12 11:53:19 +0200

    Class player

--- src/main/java/com/example/jeu6/Player.java
+++ src/main/java/com/example/jeu6/Player.java
@@ -1,6 +1,25 @@
 package com.example.jeu6;
 
 import lombok.*;
+@Getter @Setter
 public class Player {
+    Card[] hand;
+    int score;
+
+    public void playCard(){
+
+    }
+
+    public int chooseRow(){
+
+    }
+
+    public Card[] getHand(){
+
+    }
+
+    public int getScore(){
+
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.jeu6 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires lombok;
+
+
     opens com.example.jeu6 to javafx.fxml;
     exports com.example.jeu6;
 }
