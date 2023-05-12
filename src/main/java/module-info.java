07e3e5a02bf4425e739963b45ace693cfcc921a8
tp3 <juniortitcheu@gmail.com>
2023-05-12 10:38:33 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.jeu6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.jeu6 to javafx.fxml;
+    exports com.example.jeu6;
+}
