98e541cd1b70fc8f4459ff4a1d6a1c0715de4b9b
Romain-Chevrollier <romain2002.chevrollier@gmail.com>
2023-05-13 11:27:37 +0200

    Class player

--- src/main/java/com/example/jeu6/Player.java
+++ src/main/java/com/example/jeu6/Player.java
@@ -1,6 +1,16 @@
 package com.example.jeu6;
 
 import lombok.*;
+
+import java.util.ArrayList;
+
+@Getter @Setter
 public class Player {
+    ArrayList<Card> hand;
+    int score;
+
+    public void playCard(){
+
+    }
 
 }
