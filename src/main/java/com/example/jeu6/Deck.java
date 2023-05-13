3719abbebbe0aed1732a4af48c7077b849ac893e
tp3 <juniortitcheu@gmail.com>
2023-05-13 11:09:47 +0200

    junior , nicolas , romain

--- src/main/java/com/example/jeu6/Deck.java
+++ src/main/java/com/example/jeu6/Deck.java
@@ -3,39 +3,5 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-    private ArrayList<Card> cards;
 
-    public Deck() {
-        // Créer le paquet de cartes initial
-        cards = new ArrayList<>();
-        initializeDeck();
-    }
-
-    private void initializeDeck() {
-        // Ajouter les cartes au paquet (ex: numéros de cartes de 1 à 104)
-        for (int i = 1; i <= 104; i++) {
-            cards.add(new Card(i));
-        }
-    }
-
-    public void shuffle() {
-        // Mélanger les cartes du paquet de manière aléatoire
-        Collections.shuffle(cards);
-    }
-
-    public Card drawCard() {
-        // Tirer une carte du paquet et la renvoyer
-        if (isEmpty()) {
-            return null; // Pas de carte disponible, retourne null
-        } else {
-            Card card = cards.get(0); // Récupérer la première carte
-            cards.remove(0); // Retirer la carte du paquet
-            return card;
-        }
-    }
-
-    public boolean isEmpty() {
-        // Vérifier si le paquet de cartes est vide
-        return cards.isEmpty();
-    }
 }
