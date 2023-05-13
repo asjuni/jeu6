5e3f41cfd504671976897087b5778cbd6414fa4d
nicolabrico <niga61368@eleve.isep.fr>
2023-05-13 11:17:56 +0200

    row add card to row

--- src/main/java/com/example/jeu6/Row.java
+++ src/main/java/com/example/jeu6/Row.java
@@ -1,6 +1,17 @@
 package com.example.jeu6;
 
 
+import java.util.ArrayList;
+
 public class Row {
+    private ArrayList<Card> cards;
+
+    public Row() {
+        this.cards = new ArrayList<Card>(); // Initialisation de la liste de cartes dans row
+    }
+
+    public void addCardToRow(Card card) { // Ajout d'une carte à la liste de cartes dans row
+        this.cards.add(card);
+    }
 
 }
