f04a92136f5a2a76aa750b43c4b9c6f4e4d44f04
nicolabrico <niga61368@eleve.isep.fr>
2023-05-15 09:10:43 +0200

    deck v2

--- src/main/java/com/example/jeu6/Deck.java
+++ src/main/java/com/example/jeu6/Deck.java
@@ -1,6 +1,33 @@
 package com.example.jeu6;
 
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+        // Ajouter les cartes au paquet en utilisant l'enum
+        for (Card card : Card.values()) {
+            cards.add(card);
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
 }
