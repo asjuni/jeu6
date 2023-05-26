bb601b49f421ad220ab624045523d3b33d32d306
asjuni <115579494+asjuni@users.noreply.github.com>
2023-05-26 09:57:18 +0200

    Update Player.java

--- src/main/java/com/example/jeu6/PLAYER/Player.java
+++ src/main/java/com/example/jeu6/PLAYER/Player.java
@@ -6,26 +6,27 @@ import lombok.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+@Getter@Setter
 public class Player {
-    @Getter @Setter ArrayList<Card> hand;
-    @Getter @Setter int score;
+    private ArrayList<Card> hand;
+    private int score;
 
-    public int playCard(){
+    public int playCard(Row row1, Row row2, Row row3, Row row4){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Card> handPlayer = this.returnHand(); //On cherche la main du player
+        ArrayList<Card> handPlayer = getHand(); //On cherche la main du player
         System.out.println("Veuillez choisir une carte :");
         System.out.println(handPlayer);                 //On montre la main au player
         int card = scanner.nextInt();                   //Player choisit une carte
-        return (this.chooseRow(card));
+        return chooseRow(card, row1, row2, row3, row4);
     }
 
-    private int chooseRow(int card){
-        Card rowOne = row1.getLastCard();
-        Card rowTwo = row2.getLastCard();
-        Card rowThree = row3.getLastCard();
-        Card rowFour = row4.getLastCard();
-        ArrayList<Card> listOfLastCard = new ArrayList<>(Arrays.asList(rowOne,rowTwo,rowThree,rowFour));
-        for (int i =0; i < listOfLastCard.size(); i++){
+    private int chooseRow(int card, Row row1, Row row2, Row row3, Row row4){
+        Card lastCardRowOne = row1.getLastCard();
+        Card lastCardRowTwo = row2.getLastCard();
+        Card lastCardRowThree = row3.getLastCard();
+        Card lastCardRowFour = row4.getLastCard();
+        ArrayList<Card> listOfLastCard = new ArrayList<>(Arrays.asList(lastCardRowOne, lastCardRowTwo, lastCardRowThree, lastCardRowFour));
+        for (int i = 0; i < listOfLastCard.size(); i++){
             int numberCard = listOfLastCard.get(i).ordinal();
             if (card < numberCard){
                 return i - 1;
@@ -34,8 +35,19 @@ public class Player {
         return 0;
     }
 
-    private ArrayList<Card> returnHand(){
-        return this.getHand();
+    public ArrayList<Card> getHand() {
+        return hand;
     }
 
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
