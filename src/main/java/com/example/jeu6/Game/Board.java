fa199dd5cd7480a4a69ad5f6805ad979b60a74b8
asjuni <115579494+asjuni@users.noreply.github.com>
2023-05-26 11:26:34 +0200

    Update Board.java

--- src/main/java/com/example/jeu6/Game/Board.java
+++ src/main/java/com/example/jeu6/Game/Board.java
@@ -1,4 +1,24 @@
 package com.example.jeu6.Game;
 
+import com.example.jeu6.PLAYER.Row;
+
 public class Board {
+    private Row[] rows;
+
+    public Board(int numRows) {
+        rows = new Row[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new Row();
+        }
+    }
+
+    public Row[] getRows() {
+        return rows;
+    }
+
+    public void clearBoard() {
+        for (Row row : rows) {
+            row.clear();
+        }
+    }
 }
